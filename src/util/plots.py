0651098ba06145e2c2433a4724fd0627a29107b9
zeliest <48211576+zeliest@users.noreply.github.com>
2020-11-24 16:38:29 +0100

    add reference in bar plots

--- src/util/plots.py
+++ src/util/plots.py
@@ -10,13 +10,13 @@ from climada.entity import Exposures
 from scipy.sparse import csr_matrix
 
 
-def plot_impacts_heat(agg_impacts_mc, unit, uncertainty=True, color=None):
+def plot_impacts_heat(agg_impacts_mc, unit, impact_type, uncertainty=True, color=None, labels_cat=None, save=False):
     # Add a column to each dataframe with the sum of all exposures for each monte carlo.
     for s_ in agg_impacts_mc:
         for y_ in sorted(list(agg_impacts_mc[s_])):
             agg_impacts_mc[s_][y_] = pd.DataFrame.from_dict(agg_impacts_mc[s_][y_])
-            agg_impacts_mc[s_][y_]['total'] \
-                = agg_impacts_mc[s_][y_][list(agg_impacts_mc[s_][y_].columns)].sum(axis=1)
+        # agg_impacts_mc[s_][y_]['total'] \
+        #    = agg_impacts_mc[s_][y_][list(agg_impacts_mc[s_][y_].columns)].sum(axis=1)
 
     rcps = {'RCP26': 'RCP2.6', 'RCP45': 'RCP4.5', 'RCP85': 'RCP8.5'}  # to get the correct name in the plots
 
@@ -33,79 +33,24 @@ def plot_impacts_heat(agg_impacts_mc, unit, uncertainty=True, color=None):
                 s_]] = pd.DataFrame()  # dataframe containing the 5th percentile realization for the different exposures
 
             for y_ in sorted(list(agg_impacts_mc[s_])):
-                median[rcps[s_]][y_] = ((agg_impacts_mc[s_][y_].iloc[:, 0:-1]).median())  # don't need the total here
-                maximums[rcps[s_]][y_] = ((agg_impacts_mc[s_][y_].iloc[:, 0:-1]).quantile(0.95))
-                minimums[rcps[s_]][y_] = ((agg_impacts_mc[s_][y_].iloc[:, 0:-1]).quantile(0.05))
+                median[rcps[s_]][str(y_)] = ((agg_impacts_mc[s_][y_]).median())  # don't need the total here
+                maximums[rcps[s_]][str(y_)] = ((agg_impacts_mc[s_][y_]).quantile(0.95))
+                minimums[rcps[s_]][str(y_)] = ((agg_impacts_mc[s_][y_]).quantile(0.05))
 
             median[rcps[s_]] = median[rcps[s_]].transpose()
             maximums[rcps[s_]] = maximums[rcps[s_]].transpose()
             minimums[rcps[s_]] = minimums[rcps[s_]].transpose()
 
-        # plt.savefig(''.join([fig_dir,'loss_ch/predicted_loss_2020_2065.pdf']),bbox_inches='tight')
-        plot_clustered_stacked_with_error(median, minimums, maximums, color=color)
-        plt.ylabel(unit)
 
-    else:
-        for s_ in agg_impacts_mc:
-            median[rcps[s_]] = pd.DataFrame()  # dataframe containing the median realization for the different exposures
-            for y_ in sorted(list(agg_impacts_mc[s_])):
-                median[rcps[s_]][y_] = ((agg_impacts_mc[s_][y_].iloc[:, 0:-1]).median())  # don't need the total here
-            median[rcps[s_]] = median[rcps[s_]].transpose()
-
-    fig, ax = plt.subplots()
-    plot_clustered_stacked(median, title='', color=color)
-    plt.ylabel(unit)
-    ax.ticklabel_format(style='plain')
-
-def plot_clustered_stacked(dataframe_dict, title="multiple stacked bar plot", H="/", **kwargs):
-    """Given a dict of dataframes, with identical columns and index, create a clustered stacked bar plot.
-labels is a list of the names of the dataframe, used for the legend
-title is a string for the title of the plot
-H is the hatch used for identification of the different dataframe
-mostly copied from: https://stackoverflow.com/questions/22787209/how-to-have-clusters-of-stacked-bars-with-python-pandas
-"""
-
-    dfall = list(dataframe_dict[list(dataframe_dict.keys())[l_]] for l_ in range(len(dataframe_dict)))
-    labels = list(list(dataframe_dict.keys())[k_] for k_ in range(len(dataframe_dict)))
-
-    n_df = len(dfall)
-    n_col = len(dfall[0].columns)
-    n_ind = len(dfall[0].index)
-    axe = plt.subplot(111)
-
-    for df in dfall:  # for each data frame
-        axe = df.plot(kind="bar",
-                      linewidth=0,
-                      stacked=True,
-                      ax=axe,
-                      legend=False,
-                      grid=False,
-                      **kwargs, edgecolor='white', alpha=1)  # make bar plots
-
-    h, l = axe.get_legend_handles_labels()  # get the handles we want to modify
-    for i in range(0, n_df * n_col, n_col):  # len(h) = n_col * n_df
-        for j, pa in enumerate(h[i:i + n_col]):
-            for rect in pa.patches:  # for each index
-                rect.set_x(rect.get_x() + 1 / float(n_df + 1) * i / float(n_col))
-                rect.set_hatch(H * int(i / n_col))  # edited part
-                rect.set_width(1 / float(n_df + 1))
-
-    axe.set_xticks((np.arange(0, 2 * n_ind, 2) + 1 / float(n_df + 1)) / 2.)
-    axe.set_xticklabels(df.index, rotation=0)
-    axe.set_title(title)
-
-    # Add invisible data to add another legend
-    n = []
-    for i in range(n_df):
-        n.append(axe.bar(0, 0, color="silver", hatch=H * i, edgecolor='white'))
-
-    l1 = axe.legend(h[:n_col], l[:n_col], loc=[1.01, 0.5])
-    l2 = plt.legend(n, labels, loc=[1.01, 0.1])
-    axe.add_artist(l1)
-    return axe
+        if save:
+            fig1, ax1 = plot_clustered_stacked(median, minimums, maximums, unit, color=color, error_bars=False, plot_legend=False, labels_cat= labels_cat)
+            plt.savefig(''.join(['../figures/', impact_type, '_bar_2020_2080.pdf']), bbox_inches='tight')
+            fig2, ax2 = plot_clustered_stacked(median, minimums, maximums, unit, color=color, labels_cat=labels_cat)
+            plt.savefig(''.join(['../figures/', impact_type, '_bar_err_2020_2080.pdf']), bbox_inches='tight')
 
 
-def plot_clustered_stacked_with_error(median, minimums, maximums, color=None):
+def plot_clustered_stacked(medians, minimums, maximums, unit, color=None, ref_year='2020', error_bars=True,
+                           plot_legend=True, labels_cat=None):
     """Given a dict of dataframes, with identical columns and index, create a clustered stacked bar plot.
 labels is a list of the names of the dataframe, used for the legend
 H is the hatch used for identification of the different dataframe.
@@ -113,43 +58,65 @@ Minimums and maximums are also dict of dataframe with the same structure but con
 partly copied from: https://stackoverflow.com/questions/22787209/how-to-have-clusters-of-stacked-bars-with-python-pandas
 """
 
-    ax = plt.subplot(111)
-    labels_graph = list(median[list(median.keys())[0]])
-    lc = len(median[list(median.keys())[0]].columns)
-    b_width = 0.2
-    hatches = ['', '//', '//////']
+    fig, ax = plt.subplots()
+    if labels_cat is None:
+        labels_cat = list(medians[list(medians.keys())[0]])
+    len_col = len(medians[list(medians.keys())[0]].columns)
+    b_width = 0.27
+    hatches = ['', '/', '///']
     if color is None:
-        color = ["#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(lc)]
+        color = ["#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(len_col)]
     else:
         color = color
 
-    for j_, s_ in enumerate(median):
-        median[s_].columns = (i_ for i_ in range(lc))  # change the names of the columns, otherwise weird legend...
-        H = hatches[j_]
-        N = len(median[s_])
+    for scenario_num, scenario in enumerate(medians):
+        medians[scenario].columns = (column for column in
+                                     range(len_col))  # change the names of the columns, otherwise weird legend...
+        H = hatches[scenario_num]
+        N = len(medians[scenario])
         index = np.arange(N)
-        labels = median[s_].index
-        for p_, c_ in enumerate(median[s_]):
-            if p_ == 0:
-                bottom = None
+        labels = medians[scenario].index
+        for p, column in enumerate(medians[scenario]):
+            if p == 0:
+                bottom0 = None
+                bottom1 = None
             else:
-                bottom = sum(median[s_].iloc[:, b_] for b_ in range(0, p_))
-            if p_ == len(median[s_].columns) - 1:
-                err = np.array([minimums[s_].sum(axis=1).values, maximums[s_].sum(axis=1).values])
-            else:
-                err = None
-            ax.bar(index + j_ * b_width, median[s_].iloc[:, p_], data=median[s_], width=0.2, hatch=H,
-                   bottom=bottom,
-                   color=color[p_], edgecolor='white', label=labels_graph[p_], yerr=err,
-                   error_kw=dict(barsabove=True, elinewidth=1, capsize=3, ecolor='gray'))
+                bottom = sum(medians[scenario].iloc[:, b] for b in range(0, p))
+                bottom0 = bottom[0]
+                bottom1 = bottom[1:]
+            if p == len(medians[scenario].columns) - 1:
+                err = np.array([medians[scenario].sum(axis=1).values - minimums[scenario].sum(axis=1).values,
+                                maximums[scenario].sum(axis=1).values - medians[scenario].sum(axis=1).values])
+                err0 = [[err[0, 0]], [err[1, 0]]]
+                err1 = err[:, 1:]
+            if p != (len(medians[scenario].columns) - 1) or error_bars == False:
+                err0 = None
+                err1 = None
+            position = index + scenario_num * b_width
+            if scenario == 'RCP8.5':
+                bars1 = ax.bar(b_width, medians[scenario].iloc[0, p],
+                               width=b_width, hatch=H,
+                               bottom=bottom0,
+                               color=color[p], edgecolor='white', label=ref_year, yerr=err0,
+                               error_kw=dict(barsabove=True, elinewidth=1, capsize=3, ecolor='gray'))
+            bars2 = ax.bar(position[1:], medians[scenario].iloc[1:, p], data=medians[scenario].iloc[1:, p],
+                           width=b_width, hatch=H,
+                           bottom=bottom1,
+                           color=color[p], edgecolor='white', label=labels_cat[p], yerr=err1,
+                           error_kw=dict(barsabove=True, elinewidth=1, capsize=3, ecolor='gray'))
 
-        ax.set_ylabel('Annual Expected Damage')
-        if j_ == 0:
-            plt.xticks(index + b_width, labels)
+        ax.set_ylabel(unit)
 
-            ax.legend(loc=[1.01, 0.5])
-            # ax.legend(h[:lc], l[:lc], loc=[1.01, 0.5])
-            # ax.add_artist(l2)
-        median[s_].columns = labels_graph
-    return ax
+        plt.xticks(index + b_width, labels)
+        if plot_legend:
+            if scenario_num == 0:
+                dummy_bars = []
+                for i in range(len(hatches)):
+                    dummy_bars.append(ax.bar(0, 0, color="silver", hatch=hatches[i], edgecolor='white'))
+                l1 = ax.legend(bbox_to_anchor=(0.7, -0.43), loc="lower right")
+                plt.legend(dummy_bars, list(medians.keys()), bbox_to_anchor=(1, -0.43), loc="lower right")
+                l2 = ax.add_artist(l1)
 
+        medians[scenario].columns = labels_cat
+    ax.set(xlim=[-0.3, np.max(position)+0.3])
+    return fig, ax
