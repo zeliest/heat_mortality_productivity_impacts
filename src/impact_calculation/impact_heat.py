correct error in impact calculation

src/impact_calculation/impact_heat.py
@@ -236,7 +236,7 @@ class ImpactHeatMortality(Impact):
 
         max_temp = temperature_matrix.max()
         expected_deaths = np.mean([daily_deaths / imp_fun.calc_mdr(t)
-                           for t in range(22, int(np.ceil(max_temp)) + 1)])
+                           for t in range(22, int(np.ceil(max_temp)) + 1)], axis=0)
         #average_death = np.sum(np.multiply(exposure_values, expected_deaths[t]) for t in range(len(expected_deaths)))
         # Compute impact matrix
         self.imp_mat[0, exp_iimp] = self._impact_mortality(temperature_matrix, exposure_values, expected_deaths,
@@ -250,8 +250,7 @@ class ImpactHeatMortality(Impact):
         occurence = np.apply_along_axis(np.bincount, axis=0, arr=temperature_array,
                                         minlength=np.max(temperature_array) + 1)
         occurence = {t: occurence[t] for t in range(22, len(occurence))}
-        value = {t: np.multiply(1, imp_fun.calc_mdr(t) - 1) for t in temperatures}
-        af = {t: np.divide(value[t], value[t] + 1) for t in temperatures}
+        af = {t: np.divide(imp_fun.calc_mdr(t) - 1, imp_fun.calc_mdr(t)) for t in temperatures}
         return np.sum(af[t] * occurence[t] * average_death for t in temperatures)
 
 